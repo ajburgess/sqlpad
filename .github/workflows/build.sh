be23bb78deba1a2dac57341e3253cfa0b6f33cc6
Andrew Burgess <andrewjohnburgess@outlook.com>
2021-03-09 06:38:22 +0000

    python

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,7 @@
 set -x
 
 apt-get update
-apt-get install -y --no-install-recommends g++ make python python3 python3-setuptools unixodbc-dev
+apt-get install -y --no-install-recommends g++ make python python3 python3-setuptools unixodbc unixodbc-dev unixodbc-devel
 
 bash -x scripts/build.sh
 
